df963144b9f47f4fca1f0cbef6110017cf0cb602
root <root@vmi1177397.contaboserver.net>
2023-03-11 03:25:14 +0100

    Added new commit logger
    Placeholder text

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,15 +5,16 @@ import { color } from "../functions";
 const event : BotEvent = {
     name: "ready",
     once: true,
-    execute: (client : Client) => {
+    execute: async(client : Client) => {
         console.log(color("text", `💪 Logged in as ${color("variable", client.user?.tag)}`))
         client.user?.setPresence({ activities: [ { name: ".gg/azury", type: 2 } ], status: "online" })
 
         setInterval(() => { client.user?.setPresence({ activities: [ { name: client.config.Presence.Names[Math.random() * client.config.Presence.Names.length | 0], type: client.config.Presence.Type } ], status: client.config.Presence.Status }) }, 10000)
 
-        // list all updated files in a message using child_process
         const { exec } = require("child_process");
-        exec("git diff --name-only", (error: any, stdout: any, stderr: any) => {
+
+        setInterval(() => {
+        exec("git log -1 --pretty=format:'%h - %s (%an, %ar)'", async (error: any, stdout: any, stderr: any) => {
             if (error) {
                 console.log(`error: ${error.message}`);
                 return;
@@ -22,12 +23,14 @@ const event : BotEvent = {
                 console.log(`stderr: ${stderr}`);
                 return;
             }
-
-            const files = stdout.split("\n");
-            if (files.length > 0) {
-                (client.channels.cache.get("1053284414601297920") as any)?.send({ content: `**${files.length}** files have been updated:\n\`\`\`${files.join("\n")}\`\`\`` })
-            }
-        });
+            const channel = client.channels.cache.get('1053284414601297920') as any;
+            const messages = await channel.messages.fetch({ limit: 100 });
+                const filtered = messages.filter((m: any) => m.content.includes(`📝 Latest commit:\n${stdout}`));
+                if(filtered.size > 0) return;
+            console.log(color("text", `📝 Latest commit: ${color("variable", stdout)}`));
+            (client.channels.cache.get('1053284414601297920') as any)?.send({ content: `📝 Latest commit:\n${stdout}`})
+        })
+        }, 60000)
     }
 }
 
